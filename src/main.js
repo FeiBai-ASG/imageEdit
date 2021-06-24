01758c8411c9d8b821536a4eb41b962768b12c80
Sorryjjj <1528164110@qq.com>
2021-06-24 09:55:48 +0800

    feat(image url): use homework images

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './utils/rem'
 import wx from 'weixin-js-sdk'
+import { Base64 } from 'js-base64'
 
 Vue.config.productionTip = false
 Vue.prototype.wx = wx
+Vue.prototype.Base64 = Base64
 
 new Vue({
   router,
